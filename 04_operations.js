cb11993461b5190d3626c7e810e305d7ae7cab29
Akashpvt <155839828+Akashpvt@users.noreply.github.com>
2024-06-26 17:13:52 +0000

    Operation&Comparision

---
+++ 04_operations.js
@@ -0,0 +1,15 @@
+let value = 4
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Akash"
+let str3 = str1 + str2
+console.log(str3);
