7cda73ee0f0747cf2707f96ac4dd9b0bc44bd3cb
Akashpvt <155839828+Akashpvt@users.noreply.github.com>
2024-06-28 02:40:07 +0000

    Array

---
+++ Js_Series/02_basics/01_Array.js
@@ -0,0 +1,36 @@
+//Array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+// console.log(myArr[4]);
+
+//Array Methods
+
+// myArr.push(8)
+// myArr.push(5)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+
+// console.log(newArr);
+// console.log(myArr);
+
+// slice and Splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,4)
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,4)
+
+console.log("c", myArr);
+console.log(myn2);
