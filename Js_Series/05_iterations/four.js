6dcccf252d1e89bd74e78343f3e0b5cd9d97d95a
Akashpvt <155839828+Akashpvt@users.noreply.github.com>
2024-07-01 17:26:19 +0000

    forin_loop

---
+++ Js_Series/05_iterations/four.js
@@ -0,0 +1,16 @@
+const myObject = {
+    js: 'javascript',
+    cpp: "c++",
+    py: 'python',
+    rb: 'ruby'
+}
+
+for (const key in myObject) {
+  // console.log(myObject[key]);
+}
+
+const programing = ["js", "cpp", "py", 'rb']
+
+for (const key in programing) {
+   console.log(programing[key]);
+}
