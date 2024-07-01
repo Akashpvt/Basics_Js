ef0e911da609e6b610da3208b7faf11cf3fdafff
Akashpvt <155839828+Akashpvt@users.noreply.github.com>
2024-07-01 02:46:21 +0000

    one

---
+++ Js_Series/04_control_flow/one.js
@@ -0,0 +1,52 @@
+// <, >, <=, >=, ==, !=, ===, !== 
+//if
+
+// const isUserLoggenIn = true
+// const temprerature = 40
+
+// if ( temprerature === 50 ){
+//     console.log(`less then 50`);
+// }
+// else{
+//     console.log(`temperature is greete then ${50}`);
+// }
+// console.log("execute");
+
+// const score =200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+
+// }
+// console.log(`User power: ${power}`);
+
+
+// const balance = 10000
+
+// //if (balance > 5000) console.log("text"), console.log("test2");
+
+// if (balance < 5000){
+//     console.log("less tham 5000");
+// } else if (balance < 8000) {
+//     console.log("less tham 8000"); 
+// } else if (balance > 12000) {
+//     console.log("less tham 12000");
+    
+// } else{
+//     console.log("less tham 10001");
+// }
+
+const isUserLoggenIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (isUserLoggenIn && debitCard && 2==2){
+    console.log("Allow to buy coures");
+} 
+
+if (loggedInFromGoogle|| loggedInFromEmail) {
+    console.log("User logged in");
+    
+}
